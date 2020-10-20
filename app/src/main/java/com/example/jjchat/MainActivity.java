753eeaf52f98aac10e20c96155a676ee8f9e8886
josephwyhn <gaming_josi@hotmail.com>
2020-10-20 13:49:24 +0200

    WIP: Benutzeroberfläche bis zum Onclicklistener bearbeitet

--- app/src/main/java/com/example/jjchat/MainActivity.java
+++ app/src/main/java/com/example/jjchat/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.jjchat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText eBenutzername;
+    private EditText ePasswort;
+    private Button eLogin;
+    private TextView eAttemptInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eBenutzername = findViewById(R.id.etBenutzername);
+        ePasswort = findViewById(R.id.etPasswort);
+        eLogin = findViewById(R.id.btnLogin);
+        eAttemptInfo = findViewById(R.id.tvAttemptsInfo);
+
+        eLogin.setOnClickListener(new View.OnClickListener() {
+        @Override
+            public void onClick(View v){
+                String inputname = eBenutzername.getText().toString();
+            }
+        });
     }
 }
