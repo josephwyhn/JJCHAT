3223f666ea160e20e2e949a571f9b0af9b5fb6d3
Jeremy Treutler <Jeremy@fa-divers.com>
2020-12-10 16:48:12 +0100

    removed unnecessary files and other stuff

--- backend/JJChatBackend/JJChatAPI/Global.asax.cs
+++ backend/JJChatBackend/JJChatAPI/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -9,13 +6,12 @@ using System.Web.Routing;
 
 namespace JJChatAPI
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
