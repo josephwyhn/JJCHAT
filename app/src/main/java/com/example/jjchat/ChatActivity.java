54d2911cc48d9ecabd9fa359a3cc9438a94debab
josephwyhn <gaming_josi@hotmail.com>
2021-02-01 14:41:08 +0100

    SASSAS

--- app/src/main/java/com/example/jjchat/ChatActivity.java
+++ app/src/main/java/com/example/jjchat/ChatActivity.java
@@ -1,14 +1,14 @@
 package com.example.jjchat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class HomePageAcitivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page_acitivity);
+        setContentView(R.layout.activity_home_page_);
     }
 }
--- app/src/main/java/com/example/jjchat/HomePageAcitivity.java
+++ app/src/main/java/com/example/jjchat/HomePageAcitivity.java
@@ -1,14 +1,27 @@
 package com.example.jjchat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class HomePageAcitivity extends AppCompatActivity {
+ class HomePageActivity extends ListActivity {
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        // use your custom layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_home_page_, R.id.label, values);
+        setListAdapter(adapter);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page_acitivity);
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
     }
 }
